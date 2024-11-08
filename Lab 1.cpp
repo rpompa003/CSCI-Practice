be7cbe92389c87918531ac95ad587368e99f151a
rpompa003 <rpompa003@student.butte.edu>
2024-11-07 17:57:02 -0800

    Update Lab 1.cpp

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -1,40 +1,49 @@
-//,ade by rafael pompa
-// made on 11/5
-//this program is used to write to a text file and put 10 random numbers between 50 and 100.
+// Made by Rafael Pompa
+// Made on 11/5
+// This program writes 10 random numbers between 50 and 100 to a text file,
+// reads them back, and displays the total.
 
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-#include<fstream>
-#include<cmath>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
 using namespace std;
 
-void intro(){
-  cout<<"this program will read from the file and adds up all of the numbers."<<endl;
-  cout<<"Then the program should print all the numbers and display the total of the numbers added together."<<endl;
-  cout<<" "<<endl;
+void intro() {
+    cout << "This program will read from the file and add up all of the numbers." << endl;
+    cout << "Then the program will print all the numbers and display the total." << endl;
+    cout << " " << endl;
 }
 
 int main() { 
-  int max = 50;
-  int RandomNumber =0;
-  int valueArray[10];
-  int total =0;
+    int max = 50;
+    int RandomNumber = 0;
+    int valueArray[10];
+    int total = 0;
 
-intro();
+    intro();
 
-ifstream infile ("math.txt");
-srand(time(0));
-  for (int i = 0;i<10;i++){
-  valueArray[i]= RandomNumber = rand()%max +50;
-  }
-  for (int i = 0;i<10;i++){
-  cout<<valueArray[i];
-  }
+    // Open file for writing
+    ofstream outfile("histo.txt");
+    srand(time(0));
 
-while (infile >> RandomNumber) {
+    // Write 10 random numbers to file
+    for (int i = 0; i < 10; i++) {
+        valueArray[i] = rand() % max + 50;
+        outfile << valueArray[i] << " "; // Add space between numbers for readability
+    }
+    outfile.close();
+
+    // Open file for reading and calculate total
+    ifstream infile("histo.txt");
+    cout << "Numbers in the file:" << endl;
+    while (infile >> RandomNumber) {
+        cout << RandomNumber << " "; // Print each number
         total += RandomNumber;
-    cout<< total;
-}
+    }
+    infile.close();
 
+    cout << endl << "Total of numbers: " << total << endl;
+
+    return 0;
 }
